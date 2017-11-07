4c8bb51424f8f3272040e4c4940bd9afd605c145
trevm1226 <trevm12@gmail.com>
2017-11-07 14:05:00 -0600

    grid setup

--- src/sample/peachStage.java
+++ src/sample/peachStage.java
@@ -1,16 +1,27 @@
 package sample;
 
+import javafx.stage.Stage;
 import mayflower.World;
 
 public class PeachStage extends World{
     enum StageObject{
-        EMPTY,
         WALL,
         SNAKE,
         PEACH
     }
+    public StageObject[][] grid = new StageObject[30][40];
     public PeachStage()
     {
+        for(int r = 0; r < 30; r++)
+        {
+            for(int c = 0; c < 40; c++)
+            {
+                if(r == 0 || r == 29 || c == 0 || c == 39)
+                {
+                    grid[r][c] = StageObject.WALL;
+                }
+            }
+        }
         SnakeActor snek = new SnakeActor();
         addObject(snek, 20, 20);
     }

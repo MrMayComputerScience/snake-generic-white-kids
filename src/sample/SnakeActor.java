1c75ef9f11dd02e1bbf91ab9aa6fdef87b30c9d2
Cshauli17 <cshaulis.baseball17@gmail.com>
2017-11-07 13:43:21 -0600

    up key maybe

--- src/sample/SnakeActor.java
+++ src/sample/SnakeActor.java
@@ -5,19 +5,19 @@ import mayflower.*;
 
 
 
-public class SnakeActor extends Actor {
-    public SnakeActor(){
-
-    }
-
-
+public class SnakeActor extends Actor{
     public SnakeActor()
     {
         setImage("eggplantsnake.jpg");
     }
 
     public void act(){
+        
 
+        if(Mayflower.isKeyPressed(Keyboard.KEY_UP) || Mayflower.isKeyPressed(Keyboard.KEY_UP)){
+           this.move(1);
+           setRotation(90);
+        }
         
     }
 

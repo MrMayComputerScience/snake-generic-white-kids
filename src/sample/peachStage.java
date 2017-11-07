91ea1508af1c23eae10a9402e6af5f44bfb779f4
trevm1226 <trevm12@gmail.com>
2017-11-07 14:35:39 -0600

    push

--- src/sample/peachStage.java
+++ src/sample/peachStage.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.stage.Stage;
 import mayflower.World;
 
 public class PeachStage extends World{
@@ -9,7 +8,8 @@ public class PeachStage extends World{
         SNAKE,
         PEACH
     }
-    public StageObject[][] grid = new StageObject[30][40];
+    public StageObject[][] peachGrid = new StageObject[30][40];
+    final int mulitplier = 20;
     public PeachStage()
     {
         for(int r = 0; r < 30; r++)
@@ -18,12 +18,20 @@ public class PeachStage extends World{
             {
                 if(r == 0 || r == 29 || c == 0 || c == 39)
                 {
-                    grid[r][c] = StageObject.WALL;
+                    peachGrid[r][c] = StageObject.WALL;
+                    addObject(new wall(), r*mulitplier, c*mulitplier);
                 }
             }
         }
-        grid[1][1] = StageObject.SNAKE;
+        peachGrid[1][1] = StageObject.SNAKE;
+        addObject(new SnakeActor(), mulitplier, mulitplier);
     }
+
+    public void updateStage()
+    {
+
+    }
+
 
     @Override
     public void act() {

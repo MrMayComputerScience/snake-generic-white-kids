787e68b5787f6406871b6d14b1ef107d0bdcdd43
Cshauli17 <cshaulis.baseball17@gmail.com>
2017-11-07 14:17:00 -0600

    snake moves

--- src/sample/SnakeActor.java
+++ src/sample/SnakeActor.java
@@ -5,20 +5,40 @@ import mayflower.*;
 
 
 
-public class SnakeActor extends Actor{
+public class SnakeActor extends Actor implements Direction{
+    private Timer t = new Timer();
     public SnakeActor()
     {
         setImage("eggplantsnake.jpg");
+        t = new Timer(75);
     }
 
     public void act(){
-        
 
-        if(Mayflower.isKeyPressed(Keyboard.KEY_UP) || Mayflower.isKeyPressed(Keyboard.KEY_UP)){
-           this.move(1);
-           setRotation(90);
+        if(t.isDone()){
+            //System.out.println(t.toString());
+            t.reset();
+            if (Mayflower.isKeyDown(Keyboard.KEY_UP) || Mayflower.isKeyDown(Keyboard.KEY_W)) {
+                setRotation(Direction.NORTH);
+            }
+            else if (Mayflower.isKeyDown(Keyboard.KEY_DOWN) || Mayflower.isKeyDown(Keyboard.KEY_S)) {
+                setRotation(Direction.SOUTH);
+            }
+            else if (Mayflower.isKeyDown(Keyboard.KEY_LEFT) || Mayflower.isKeyDown(Keyboard.KEY_A)) {
+                setRotation(Direction.WEST);
+            }
+            else if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT) || Mayflower.isKeyDown(Keyboard.KEY_D)) {
+                setRotation(Direction.EAST);
+            }
+            move(20);
         }
-        
+
+
+
+
+
+
+
     }
 
 }

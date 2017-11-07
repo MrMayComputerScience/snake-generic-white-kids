e474944979f0834ea6b3af1b8845b057e9c40d8a
Mason McCully <mmccully2000@gmail.com>
2017-11-07 13:29:31 -0600

    gfd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,17 @@
 package sample;
+import mayflower.*;
+import mayflower.test.TestWorld;
+import mayflower.test.astar.MyWorld;
 
-public class Main {
+public class Main{
+    public static void main(String[] args){
+        Mayflower may = new Mayflower("Test", 800, 600){
+            @Override
+            public void init() {
+                setWorld(new TestWorld());
+            }
+        };
+        may.init();
+    }
 
 }

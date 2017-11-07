5633d20def8a470d6358b84604ab5008fb80b83b
Mason McCully <mmccully2000@gmail.com>
2017-11-07 13:34:17 -0600

    Revert "Re-init"
    This reverts commit a284853b42b19a98ac490eba99f15dcffe7a050b.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,5 +8,27 @@ public class Main{
 
     }
 
+<<<<<<< HEAD
+=======
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+>>>>>>> parent of a284853... Re-init
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }

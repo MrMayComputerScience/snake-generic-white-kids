b368c3b74d10792a0320918802d3a77d7791ad11
Cshauli17 <cshaulis.baseball17@gmail.com>
2017-11-09 13:39:31 -0600

    tit screen

---
+++ src/sample/LANButton.java
@@ -0,0 +1,15 @@
+package sample;
+
+import mayflower.Mayflower;
+
+public class LANButton extends Button{
+    public LANButton(String name){
+        super(name);
+    }
+    public void OnClick(){
+
+       // Mayflower.setWorld(new peachStage());
+
+
+    }
+}
---
+++ src/sample/SinglePlayerButton.java
@@ -0,0 +1,15 @@
+package sample;
+
+import mayflower.*;
+
+public class SinglePlayerButton extends Button{
+    public SinglePlayerButton(String name){
+        super(name);
+    }
+    public void OnClick(){
+
+        Mayflower.setWorld(new peachStage());
+
+
+    }
+}

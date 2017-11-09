7d5ad1a770f7598d515555f37d093dd3d6cffe07
Cshauli17 <cshaulis.baseball17@gmail.com>
2017-11-09 14:13:26 -0600

    gameover

---
+++ src/sample/MultiplayerButton.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class MultiplayerButton extends Button {
+    public MultiplayerButton(String name){
+        super(name);
+    }
+    public void OnClick(){
+
+    }
+}
--- src/sample/PlayAgainButton.java
+++ src/sample/PlayAgainButton.java
@@ -1,15 +1,11 @@
 package sample;
-
 import mayflower.*;
-
-public class SinglePlayerButton extends Button{
-    public SinglePlayerButton(String name){
+public class PlayAgainButton extends Button {
+    public PlayAgainButton(String name){
         super(name);
     }
     public void OnClick(){
-
         Mayflower.setWorld(new PeachStage());
-
-
     }
+
 }

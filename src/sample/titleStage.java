eab29ae69e6326418d4046051d6868a9472816de
Matteson.Trevor.s631741 <s631741@rrisd.local>
2017-11-30 14:22:47 -0600

    wha

--- src/sample/titleStage.java
+++ src/sample/titleStage.java
@@ -4,13 +4,13 @@ import mayflower.*;
 public class titleStage extends World{
     Button fpButton = new SinglePlayerButton("StartSinglePlayer.jpg");
     Button lanButton = new LANButton("NotImplemented.png");
-    Button mpButton = new MultiplayerButton("NotImplemented.png");
+    //Button mpButton = new MultiplayerButton("NotImplemented.png");
     Button hsButton = new HighScoreButton("HighScoreButton.png");
     public titleStage(){
         setBackground("title.png");
         addObject(fpButton, 25,450);
         addObject(lanButton, 225,450);
-        addObject(mpButton, 425,450);
+        //addObject(mpButton, 425,450);
         addObject(hsButton, 625, 450);
     }
     public void act(){

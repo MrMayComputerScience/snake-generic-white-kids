1e5eb005229af6462711ec12c150da7f5758af6d
Mason McCully <mmccully2000@gmail.com>
2017-12-08 14:07:43 -0600

    HSB and Twitch

--- src/sample/TwitchSnakeActor.java
+++ src/sample/TwitchSnakeActor.java
@@ -9,39 +9,56 @@ import java.io.IOException;
 
 public class TwitchSnakeActor extends SnakeActor {
     private int num;
+    private boolean isRunning;
+    private boolean wasRunning;
     public TwitchSnakeActor(int numPlayers, int id){
         super(id);
+        isRunning = false;
+        wasRunning = false;
         num = numPlayers;
     }
     public void moveSnake(){
         if(num == 3){
             if(Mayflower.isKeyDown(Keyboard.KEY_W) && Mayflower.isKeyDown(Keyboard.KEY_Y) && Mayflower.isKeyDown(Keyboard.KEY_P)){
                 setRotation(Direction.NORTH);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_S) && Mayflower.isKeyDown(Keyboard.KEY_H) && Mayflower.isKeyDown(Keyboard.KEY_SEMICOLON)){
                 setRotation(Direction.SOUTH);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_A) && Mayflower.isKeyDown(Keyboard.KEY_G) && Mayflower.isKeyDown(Keyboard.KEY_L)){
                 setRotation(Direction.WEST);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_D) && Mayflower.isKeyDown(Keyboard.KEY_J) && Mayflower.isKeyDown(Keyboard.KEY_APOSTROPHE)){
                 setRotation(Direction.EAST);
+                isRunning = true;
             }
         }
         if(num == 4){
             if(Mayflower.isKeyDown(Keyboard.KEY_W) && Mayflower.isKeyDown(Keyboard.KEY_Y) && Mayflower.isKeyDown(Keyboard.KEY_P) && Mayflower.isKeyDown(Keyboard.KEY_UP)){
                 setRotation(Direction.NORTH);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_S) && Mayflower.isKeyDown(Keyboard.KEY_H) && Mayflower.isKeyDown(Keyboard.KEY_SEMICOLON) && Mayflower.isKeyDown(Keyboard.KEY_DOWN)){
                 setRotation(Direction.SOUTH);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_A) && Mayflower.isKeyDown(Keyboard.KEY_G) && Mayflower.isKeyDown(Keyboard.KEY_L) && Mayflower.isKeyDown(Keyboard.KEY_LEFT)){
                 setRotation(Direction.WEST);
+                isRunning = true;
             }
             else if(Mayflower.isKeyDown(Keyboard.KEY_D) && Mayflower.isKeyDown(Keyboard.KEY_J) && Mayflower.isKeyDown(Keyboard.KEY_APOSTROPHE) && Mayflower.isKeyDown(Keyboard.KEY_RIGHT)){
                 setRotation(Direction.EAST);
+                isRunning = true;
             }
         }
+        if(wasRunning != isRunning){
+            startTimer();
+            System.out.println("DIFF");
+        }
+        wasRunning = isRunning;
 
     }
     public int getNumPlayers(){
@@ -67,5 +84,11 @@ public class TwitchSnakeActor extends SnakeActor {
             e.printStackTrace();
 
         }
+    }
+    @Override
+    public void act(){
+        super.act();
+        if(!isRunning)
+            moveSnake();
     }
 }

7e1e1556c5fa3e0a33520e3718d49c2ce6cff5da
Matteson.Trevor.s631741 <s631741@rrisd.local>
2018-01-17 13:31:32 -0600

    New Snake stuff
    Edit Snake to work correctly

--- src/sample/SnakeActor.java
+++ src/sample/SnakeActor.java
@@ -29,9 +29,13 @@ public class SnakeActor extends Actor{
     private boolean running;
     private World myWorld;
     private boolean tronMode;
+    private int score;
     private GameInfo info;
+    private int x;
+    private int y;
     public SnakeActor(int di, GameInfo info)
     {
+        score = 0;
         this.info = info;
         tronMode = false;
         id = di;
@@ -147,30 +151,7 @@ public class SnakeActor extends Actor{
                 lengthToAdd--;
         }
 
-        if(t.isDone()){
-            int trueTime = (int)(System.currentTimeMillis() - timeLastUpdate);
-            int diff = trueTime - tickLen;
-            tickLen = TICK_TIME - diff;
-            sumTimes += trueTime;
-            numTicks++;
-            t.set(TICK_TIME - diff);
-            timeLastUpdate = System.currentTimeMillis();
-            int headX = getX();
-            int headY = getY();
-            int headRot = getRotation();
-            bigify();
-            t.reset();
-            moveSnake();
-            if(Math.abs(getRotation()-headRot) % 180 == 0 && tailLength >= 2)
-                setRotation(headRot);
-            time++;
-            //System.out.println(t.toString());
-            move(20);
-            handleTail(headX, headY, headRot);
-            eatPeach(detectPeach());
 
-
-        }
         if(isTouching(wall.class) || isTouching(SnakeTail.class) || isTouching(SnakeActor.class)){
             myWorld = getWorld();
             if(isTouching(SnakeActor.class)){
@@ -189,6 +170,92 @@ public class SnakeActor extends Actor{
         }
 
     }
+
+    public void turnLeft()
+    {
+        setRotation(Direction.WEST);
+    }
+
+    public void turnRight()
+    {
+        setRotation(Direction.EAST);
+    }
+
+    public void turnUp()
+    {
+        setRotation(Direction.NORTH);
+    }
+
+    public void turnDown()
+    {
+        setRotation(Direction.SOUTH);
+    }
+
+    public void tick()
+    {
+        int trueTime = (int)(System.currentTimeMillis() - timeLastUpdate);
+        int diff = trueTime - tickLen;
+        tickLen = TICK_TIME - diff;
+        sumTimes += trueTime;
+        numTicks++;
+        t.set(TICK_TIME - diff);
+        timeLastUpdate = System.currentTimeMillis();
+        int headX = getX();
+        int headY = getY();
+        int headRot = getRotation();
+        bigify();
+        t.reset();
+        moveSnake();
+        if(Math.abs(getRotation()-headRot) % 180 == 0 && tailLength >= 2)
+            setRotation(headRot);
+        time++;
+        //System.out.println(t.toString());
+        move(20);
+        handleTail(headX, headY, headRot);
+        eatPeach(detectPeach());
+    }
+
+    public void die()
+    {
+        myWorld.removeObject(this);
+    }
+
+    public void grow()
+    {
+        if(tailLength > tail.size()){
+            SnakeTail t = new SnakeTail(id, info);
+            tail.add(t);
+            getWorld().addObject(t,getX(),getY());
+        }
+    }
+
+    public void collect(@Nullable Peach peach)
+    {
+        if(peach == null){
+            //    System.out.println("Peach is null");
+            return;
+        }
+        getWorld().removeObject(peach);
+        tailLength += lengthToAdd;
+        Peach.addRandomPeach((peachStage)getWorld()); //Should never throw invalid cast errors
+    }
+
+    public void increaseScore()
+    {
+        score++;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void teleport(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
     public void removeTail(){
         for(SnakeTail t : tail){
             myWorld.removeObject(t);

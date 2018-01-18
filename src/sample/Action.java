9ef8153d64a7aae422ab83be1f1e0cfa8f3add8f
Mason McCully <mmccully2000@gmail.com>
2018-01-17 19:18:34 -0600

    Action enum implemented

--- src/sample/Action.java
+++ src/sample/Action.java
@@ -1,5 +1,32 @@
 package sample;
 
-public interface Action {
-    String P1_UP = "p1_up";
+public enum Action {
+    P1_UP("P1U"),
+    P1_LEFT("P1L"),
+    P1_DOWN("P1D"),
+    P1_RIGHT("P1R"),
+    P2_UP("P2U"),
+    P2_LEFT("P2L"),
+    P2_DOWN("P2D"),
+    P2_RIGHT("P2R"),
+    P3_UP("P3U"),
+    P3_LEFT("P3L"),
+    P3_DOWN("P3D"),
+    P3_RIGHT("P3R"),
+    P4_UP("P4U"),
+    P4_LEFT("P4L"),
+    P4_DOWN("P4D"),
+    P4_RIGHT("P4R"),
+    TICK("TIC"),
+    DIE("DIE"),
+    COLLECT("COL"),
+    GROW("GRO");
+    String name;
+    Action(String name){
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
 }
